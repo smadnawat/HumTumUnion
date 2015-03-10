c738f3b1a5453b8538da2447c673818f58860c0c
Jai Rajput <jai.rajput@mobiloittegroup.com>
2015-03-10 16:53:33 +0530

    syntax error resolved on User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
               if @user.save       
                   redirect_to @user
               else
-                  render "new"
+                  render  new_user_path
               end
             else
               @user.errors.add(:dob, "should be over 18 years")
