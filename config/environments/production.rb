3ff7143239f5ca57dfcbb5c53cd421fd28af0cc2
Jai Rajput <jai.rajput@SYS-88.(none)>
2015-04-20 21:21:30 +0530

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Signup::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
- config.action_mailer.default_url_options = { :host => 'jai-signup-app.heroku.com' }
+ config.action_mailer.default_url_options = { :host => 'humtumunion.herokuapp.com' }
 end
