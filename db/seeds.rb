09ca8bb7e014edce471400cf2ca004019d87ccb3
Jai Rajput <jai.rajput@SYS-88.(none)>
2015-04-30 12:13:50 +0530

    new updates seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Notification.update_all(:pending=> true,:message => "check your old notification")
